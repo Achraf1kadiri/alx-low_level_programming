0ec977052aed592761b38d07262fa53c1f9e2b62
Achraf1kadiri <achraf1kadiri@gmail.com>
2023-07-14 15:53:24 -0700

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-
-/**
+i/**
  * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
@@ -17,26 +15,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
-3-print_alphabets.c
-
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
 }
