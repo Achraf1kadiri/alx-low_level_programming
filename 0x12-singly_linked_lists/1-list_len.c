9388a579104039cef9878af2960485e35af5950e
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-08-24 12:31:41 -0700

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * list_len - calculates and returns the count of elements in a linked list
+ * @h: pointer to the list_t structure representing the list
+ * Returns: count of elements in the given list
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t a = 0;
 
 	while (h)
 	{
-		n++;
+		a++;
 		h = h->next;
 	}
-	return (n);
+	return (a);
 }
 
