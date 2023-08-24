c2877f916e03af3487b7718cea71897a92ddf437
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-08-24 12:35:16 -0700

    fixxxxx

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,6 +4,7 @@
 /**
  * list_len - calculates and returns the count of elements in a linked list
  * @h: pointer to the list_t structure representing the list
+ *
  * Returns: count of elements in the given list
  */
 size_t list_len(const list_t *h)
