c075a9e64830f7bcced7ec1494d950509bf05e27
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-08-25 04:16:00 -0700

    2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_list - displays all elements stored in a linked list
+ * @h: pointer to the list_t structure representing the list
  *
- * Return: the number of nodes printed
+ * Returns: the count of nodes that have been printed
  */
 size_t print_list(const list_t *h)
 {
