3eb237ea590aff13f3098323ec170f1d351d5325
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-09-28 04:31:48 -0700

    newfile

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - Recursively prints a string.
+ * @s: Argument of the function.
+ * Return: Returns void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
+
