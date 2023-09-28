7b75703fc36502dc6f060fd5091a2baa6c588c45
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-09-28 04:41:17 -0700

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - Recursively prints a string.
  * @s: Argument of the function.
@@ -7,13 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
