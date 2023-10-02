21ff823a5a7dc3cd0af4ae2dd49433508d51b50e
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-10-02 07:45:34 -0700

    update

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,7 +10,7 @@
 int _atoi(char *s)
 {
 	int sign = 1, i = 0;
-	unsigned int res = 0;
+	unsigned int result = 0;
 
 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
@@ -20,9 +20,9 @@ int _atoi(char *s)
 	}
 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-		res = (res * 10) + (s[i] - '0');
+		result = (result * 10) + (s[i] - '0');
 		i++;
 	}
-	res *= sign;
-	return (res);
+	result *= sign;
+	return (result);
 }
