795f0db1dffc5881dab43197b3e12e2cd83928c3
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-10-19 07:22:58 -0700

    opt fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,16 +10,15 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node = 0;
+	size_t node = 0;
 
-    for (; h; h = (*h).next, node++)
-    {
-        if ((*h).str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", (*h).len, (*h).str);
-    }
+	for (; h; h = (*h).next, node++)
+	{
+		if ((*h).str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", (*h).len, (*h).str);
+	}
 
-    return node;
+	return (node);
 }
-
