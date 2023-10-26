1a3e683fdbc372cbfce828fd5f3b0375c457a227
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-10-26 03:11:01 -0700

    fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,11 +11,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
-	{
+	{	
 		return (-1);
 	}
-	else if (*n |= (1 << index))
-	{
-		return (1);
-	}
+	*n |= (1 << index);
+	return (1);
 }
